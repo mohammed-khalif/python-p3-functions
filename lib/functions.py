ff65f9ea6f537a7357999f3ead3971221b97fe73
mohammed-khalif <mohamed.yusuf@student.moringaschool.com>
2023-05-15 15:26:30 -0400

    completed

--- lib/functions.py
+++ lib/functions.py
@@ -14,3 +14,31 @@ def add(num1, num2):
 
 def halve(number):
     pass
+
+# Define a method greet_programmer() that takes no arguments.
+# It should output the string "Hello, programmer!" to the terminal with print().
+def greet_programmer():
+    print("Hello, programmer!")
+
+# Define a method greet() that takes one argument, a name.
+# It should output the string "Hello, name!" to the terminal with print().
+def greet(name):
+    print(f"Hello, {name}!")
+
+# Define a method greet_with_default() that takes one argument, a name.
+# It should output the string "Hello, name!" to the terminal with print().
+# If no arguments are passed in, it should output the string "Hello, programmer!".
+def greet_with_default(name="programmer"):
+    print(f"Hello, {name}!")
+
+# Define a method add() that takes two numbers as arguments and returns the sum of those two numbers.
+def add(num1, num2):
+    return num1 + num2
+
+# Define a method halve() that takes one number as an argument and returns that number's value, divided by two.
+# If the function is called with an argument that isn't a number, it should return None.
+def halve(number):
+    if not isinstance(number, (int, float)):
+        return None
+
+    return number / 2
